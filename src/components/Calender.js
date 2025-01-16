6cc3eae7bc0125048bb144289c2ff1d0b03a635f
chandra mahesh <chandachandramahesh@gmail.com>
2025-01-16 18:50:55 +0530

    Implemented Modals that choose edit date and edit all days

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -3,29 +3,54 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import {Modal, Button, DatePicker, Form, Space, TimePicker} from 'antd';
+import {Modal, Button, Form, Space, TimePicker, Calendar} from 'antd';
 import dayjs from "dayjs";
+import { CloseOutlined } from '@ant-design/icons';
 
 
 const Calender = () => {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isOptionsModelVisible, setIsOptionsModelVisible] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [isCalendarVisible, setIsCalendarVisible]= useState(true);
   const [timeSlots, setTimeSlots] = useState([{ key: Date.now(), startTime: null, endTime: null }]);
+  const [events, setEvents] = useState([]);
 
   const handleDateClick =(info)=>{
     setSelectedDate(dayjs(info.dateStr));
-    showModel();
+    const day = getDayFromDate(info.dateStr)
+    setSelectedDay(day);
+    setIsOptionsModelVisible(true);
+   
   }
 
-  const showModel = ()=>{
+  const handleEditDateClick = ()=>{
     setIsModalVisible(true);
+    setIsOptionsModelVisible(false);
+    setIsCalendarVisible(true);
   }
 
+  const handleEditAllDaysClick = ()=>{
+    setIsModalVisible(true);
+    setIsCalendarVisible(false);
+    setIsOptionsModelVisible(false);
+  }
+
+  const getDayFromDate = (dateString) => {
+    const date = new Date(dateString);
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return days[date.getDay()];
+  };
   const handleCancel = () => {
     setIsModalVisible(false);
     setTimeSlots([{ key: Date.now(), startTime: null, endTime: null }]);
   };
+
+  const handleOptionsCancel = ()=>{
+    setIsOptionsModelVisible(false);
+  }
 
   const addTimeSlot = () => {
     setTimeSlots([...timeSlots, { key: Date.now(), startTime: null, endTime: null }]);
@@ -38,29 +63,66 @@ const Calender = () => {
   };
 
   const handleApply = () => {
-    console.log("Selected Date:", selectedDate);
-    console.log("Time Slots:", timeSlots);
-    // TODO: Update FullCalendar events here
+    const formattedSlots = timeSlots.map((slot) => ({
+      title: `${slot.startTime ? slot.startTime.format("HH:mm") : "Unknown"} - ${
+        slot.endTime ? slot.endTime.format("HH:mm") : "Unknown"
+      }`,
+      start: selectedDate.toDate(), // Set the start of the event to the selected date
+      extendedProps: {
+        startTime: slot.startTime ? slot.startTime.format("HH:mm") : "Unknown",
+        endTime: slot.endTime ? slot.endTime.format("HH:mm") : "Unknown",
+      },
+    }));
+
+    // Add new events to the state
+    setTimeSlots([{ key: Date.now(), startTime: null, endTime: null }]);
+    setEvents((prevEvents) => [...prevEvents, ...formattedSlots]);
     setIsModalVisible(false);
   };
+
+  const handleTimeSlotDelete = (key)=>{
+    if(timeSlots.length !== 1){
+    const updatedSlots =
+        timeSlots.filter((slot)=>(slot.key !== key))
+        setTimeSlots(updatedSlots);
+    }
+    else{
+      return;
+    }
+  }
 
 
 
   return (
-    <div>
+    <div    style={{ width: "70%", height: "80vh", margin: "0 auto", marginTop: '10px' }}>
     <FullCalendar plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
     initialView={'dayGridMonth'}
+    events={events}
     headerToolbar={{
       start: "today prev,next",
       center: "title",
       end: "dayGridMonth,timeGridWeek,timeGridDay"
     }}
-    height={"90vh"}
     dateClick={handleDateClick}
+     eventContent={(eventInfo) =>(
+          <div>
+            {eventInfo.event.extendedProps.startTime} - {eventInfo.event.extendedProps.endTime}
+          </div>
+        )}
     /> 
+
+    <Modal title={'Choose between:'} 
+      open={isOptionsModelVisible}
+      onCancel={handleOptionsCancel}
+      >
+        <div>
+          <p onClick={handleEditDateClick} style={{cursor:'pointer'}}>Edit date</p>
+          <p onClick={handleEditAllDaysClick} style={{cursor:'pointer'}}>Edit all {selectedDay}s</p>
+        </div>
+    </Modal>
    
    <Modal
-        title={`Edit Date: ${selectedDate}`}
+        title={isCalendarVisible ? `Edit Date: ${selectedDate}`: `Edit All ${selectedDay}s`}
         open={isModalVisible}
         onCancel={handleCancel}
         footer={[
@@ -69,14 +131,16 @@ const Calender = () => {
           </Button>,
           <Button key="apply" type="primary" onClick={handleApply}>
             Apply
-          </Button>,
+          </Button>
         ]}
       >
-        {/* Small Calendar */}
-        <DatePicker defaultValue={selectedDate ? selectedDate : null}/>
+      {
+     isCalendarVisible &&  <Calendar fullscreen={false} value={selectedDate}/>
+      }
 
+      <div>Choose your availability timings:</div>
         {/* Time Slots */}
-        <Form layout="vertical">
+        <Form style={{marginTop: 8}} layout="vertical">
           {timeSlots.map((slot, index) => (
             <Space key={slot.key} style={{ display: "flex", marginBottom: 8 }} align="baseline">
               <TimePicker
@@ -89,6 +153,7 @@ const Calender = () => {
                 value={slot.endTime}
                 onChange={(value) => handleTimeChange(index, "endTime", value)}
               />
+               <CloseOutlined onClick={()=>{handleTimeSlotDelete(slot.key)}} />
               {index === timeSlots.length - 1 && (
                 <Button type="dashed" onClick={addTimeSlot}>
                   +
